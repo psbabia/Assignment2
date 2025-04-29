163cf5aae7c73e2a98c1157ee85f52d01a31c760
psbabia <abhiprakash1979@gmail.com>
2025-04-29 21:26:56 +0530

    Add files via upload

---
+++ Ass2_Task1.py
@@ -0,0 +1,11 @@
+# Step 1: Take integer input from the user
+number = int(input("Enter an integer: "))
+
+# Step 2: Check if the number is even or odd
+if number % 2 == 0:
+    # Step 3: Display result if even
+    print(number,' is an even number.')
+else:
+    # Step 3: Display result if odd
+    print(number ,' is an odd number.')
+
---
+++ Ass2_Task2.py
@@ -0,0 +1,9 @@
+# Step 1: Initialize sum variable
+total_sum = 0
+
+# Step 2: Use a for loop to iterate from 1 to 50
+for number in range(1, 51):
+    total_sum += number  # Add each number to the sum
+
+# Step 3: Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
